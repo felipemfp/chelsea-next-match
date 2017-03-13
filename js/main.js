117dd13a95907f90d16fffbb6b06487cefce20cf
Felipe Pontes <felipemfpontes@gmail.com>
2017-03-13 06:40:04 -0300

    Add a little bit of ES6

--- js/main.js
+++ js/main.js
@@ -7,32 +7,35 @@
   const TOKEN = '41c18856748a4f86b1fd8bcd4f3b6d67'
 
   const Team = React.createClass({
-    getInfo: function (url) {
+    getInfo (url) {
       if (url.indexOf('http:') > -1) {
         url = url.replace('http:', 'https:')
       }
       axios.get(url, {
         headers: { 'X-Auth-Token': TOKEN }
-      }).then(function (response) {
+      }).then(response => {
         const data = response.data
         this.setState({
           crest: data.crestUrl,
           name: data.name
         })
-      }.bind(this))
+      })
     },
-    getInitialState: function () {
+
+    getInitialState () {
       return {
         crest: '',
         name: ''
       }
     },
-    componentWillReceiveProps: function (nextProps) {
+
+    componentWillReceiveProps (nextProps) {
       if (nextProps.url) {
         this.getInfo(nextProps.url)
       }
     },
-    render: function () {
+
+    render () {
       if (this.props.isHome) {
         return (
           <span className='team'>
@@ -48,44 +51,36 @@
     }
   })
 
-  const DateDisplay = React.createClass({
-    render: function () {
-      var dt = moment(this.props.date)
-      return (
-        <div className='dateDisplay' title={dt.format('LLLL')}>
-          {this.props.date ? dt.fromNow() : 'loading'}
-        </div>
-      )
-    }
-  })
+  const DateDisplay = ({date}) => {
+    const dt = moment(date)
+    return (
+      <div className='dateDisplay' title={dt.format('LLLL')}>
+        {date ? dt.fromNow() : 'loading'}
+      </div>
+    )
+  }
 
-  const LastGameContainer = React.createClass({
-    render: function () {
-      return (
-        <div className='lastGames container'>
-          <h6>Check out the last games</h6>
-          {
-              this.props.lastGames.map(function (lastGame) {
-                return (
-                  <div className='row' title={moment(lastGame.date).format('LLLL')}>
-                    <div className='four columns'><b>{lastGame.homeTeamName}</b></div>
-                    <div className='four columns'>
-                      <span>{lastGame.result.goalsHomeTeam}</span>
-                      <span>&nbsp;x&nbsp;</span>
-                      <span>{lastGame.result.goalsAwayTeam}</span>
-                    </div>
-                    <div className='four columns'><b>{lastGame.awayTeamName}</b></div>
-                  </div>
-                )
-              })
-            }
-        </div>
-      )
-    }
-  })
+  const LastGameContainer = ({lastGames}) => (
+    <div className='lastGames container'>
+      <h6>Check out the last games</h6>
+      {
+          lastGames.map(lastGame => (
+            <div className='row' title={moment(lastGame.date).format('LLLL')}>
+              <div className='four columns'><b>{lastGame.homeTeamName}</b></div>
+              <div className='four columns'>
+                <span>{lastGame.result.goalsHomeTeam}</span>
+                <span>&nbsp;x&nbsp;</span>
+                <span>{lastGame.result.goalsAwayTeam}</span>
+              </div>
+              <div className='four columns'><b>{lastGame.awayTeamName}</b></div>
+            </div>
+          ))
+        }
+    </div>
+  )
 
   const Board = React.createClass({
-    getInitialState: function () {
+    getInitialState () {
       return {
         date: '',
         homeTeamUrl: '',
@@ -93,10 +88,11 @@
         lastGames: []
       }
     },
-    componentDidMount: function () {
+
+    componentDidMount () {
       axios.get(this.props.url, {
         headers: { 'X-Auth-Token': TOKEN }
-      }).then(function (response) {
+      }).then(response => {
         const data = response.data
         for (let i = 0; i < data.count; i++) {
           let date = data.fixtures[i].date
@@ -113,9 +109,10 @@
             this.setState({lastGames: lastGames.slice(0, 3)})
           }
         }
-      }.bind(this))
+      })
     },
-    render: function () {
+
+    render () {
       return (
         <section className='board'>
           <DateDisplay date={this.state.date} />
